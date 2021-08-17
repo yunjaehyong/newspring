ccbe915be4f0900ed341b6ac53882bb22746536c
swempire <ldk@swempire.co.kr>
2021-08-17 11:15:54 +0900

    불필요파일 삭제

--- src/main/java/co/kr/swempire/yjh/dto/LoginDTO.java
+++ src/main/java/co/kr/swempire/yjh/dto/LoginDTO.java
@@ -1,16 +1,16 @@
-package co.kr.swempire.yjh.dto;
-
-import java.util.Map;
-
-import org.springframework.stereotype.Repository;
-
-import co.kr.swempire.yjh.vo.LoginVO;
-import co.kr.swempire.yjh.vo.YjhVO;
-
-@Repository
-public interface LoginDTO {
-
-	public Integer insertlogin(LoginVO vo);
-	
-	public Map<Object, Object> insertloginData(LoginVO vo);
-}
+//package co.kr.swempire.yjh.dto;
+//
+//import java.util.Map;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import co.kr.swempire.yjh.vo.LoginVO;
+//import co.kr.swempire.yjh.vo.YjhVO;
+//
+//@Repository
+//public interface LoginDTO {
+//
+//	public Integer insertlogin(LoginVO vo);
+//	
+//	public Map<Object, Object> insertloginData(LoginVO vo);
+//}
--- src/main/java/co/kr/swempire/yjh/vo/LoginVO.java
+++ src/main/java/co/kr/swempire/yjh/vo/LoginVO.java
@@ -1,30 +1,30 @@
-package co.kr.swempire.yjh.vo;
-
-public class LoginVO {
-
-	int loin;
-	String login_id;
-	String login_pw;
-	@Override
-	public String toString() {
-		return "loginVO [loin=" + loin + ", login_id=" + login_id + ", login_pw=" + login_pw + "]";
-	}
-	public int getloin() {
-		return loin;
-	}
-	public void setloin(int loin) {
-		this.loin = loin;
-	}
-	public String getlogin_Id() {
-		return login_id;
-	}
-	public void setId(String login_id) {
-		this.login_id = login_id;
-	}
-	public String getlogin_pw() {
-		return login_pw;
-	}
-	public void setlogin_pw(String login_pw) {
-		this.login_pw = login_pw;
-	}
-}
+//package co.kr.swempire.yjh.vo;
+//
+//public class LoginVO {
+//
+//	int loin;
+//	String login_id;
+//	String login_pw;
+//	@Override
+//	public String toString() {
+//		return "loginVO [loin=" + loin + ", login_id=" + login_id + ", login_pw=" + login_pw + "]";
+//	}
+//	public int getloin() {
+//		return loin;
+//	}
+//	public void setloin(int loin) {
+//		this.loin = loin;
+//	}
+//	public String getlogin_Id() {
+//		return login_id;
+//	}
+//	public void setId(String login_id) {
+//		this.login_id = login_id;
+//	}
+//	public String getlogin_pw() {
+//		return login_pw;
+//	}
+//	public void setlogin_pw(String login_pw) {
+//		this.login_pw = login_pw;
+//	}
+//}
